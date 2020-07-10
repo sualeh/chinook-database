2210a4fde125f8fbad319e85f535ff3732a1aa97
Sualeh Fatehi <sualeh@hotmail.com>
2020-07-10 16:25:32 -0400

    Add PostgreSQL test

---
+++ src/test/java/us/fatehi/test/TestPostgreSQL.java
@@ -0,0 +1,66 @@
+/*
+========================================================================
+Chinook Database
+http://www.schemacrawler.com
+Copyright (c) 2020, Sualeh Fatehi <sualeh@hotmail.com>.
+All rights reserved.
+------------------------------------------------------------------------
+
+This software is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+This software and the accompanying materials are made available under
+the terms of the Eclipse Public License v1.0.
+
+The Eclipse Public License is available at:
+http://www.eclipse.org/legal/epl-v10.html
+
+========================================================================
+*/
+package us.fatehi.test;
+
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.testcontainers.containers.Db2Container;
+import org.testcontainers.containers.JdbcDatabaseContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@Testcontainers(disabledWithoutDocker = true)
+public class TestPostgreSQL
+{
+
+  @Container
+  private JdbcDatabaseContainer dbContainer = new PostgreSQLContainer<>()
+    .withInitScript("chinook_database/Chinook_PostgreSql.sql");
+
+  @Test
+  public void postgreSQL()
+    throws SQLException
+  {
+    final Connection connection = dbContainer.createConnection("");
+    assertThat(connection, is(not(nullValue())));
+    assertThat(connection.isClosed(), is(false));
+
+    final JdbcTemplate jdbcTemplate =
+      new JdbcTemplate(new SingleConnectionDataSource(connection, true));
+    final Integer count = jdbcTemplate.queryForObject(
+      "SELECT COUNT(*) FROM \"Album\"",
+      Integer.class);
+    assertThat(count, is(not(nullValue())));
+    assertThat(count, is(347));
+  }
+
+}
