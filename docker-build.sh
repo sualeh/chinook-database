39af0b553fc019165f20801194e227307bab93a3
Sualeh Fatehi <sualeh@hotmail.com>
2020-07-13 18:01:12 -0400

    Docker build script

---
+++ docker-build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+CHINOOK_DATABASE_VERSION=1.0.0
+
+echo "** Creating Chinook Database v$CHINOOK_DATABASE_VERSION Docker container"
+
+# Print Docker version
+pwd
+docker version
+
+# Build Docker image
+docker build -t schemacrawler/chinook-database .
+docker tag schemacrawler/chinook-database schemacrawler/chinook-database:v$CHINOOK_DATABASE_VERSION
+docker tag schemacrawler/chinook-database schemacrawler/chinook-database:latest
+
+# Deploy image to Docker Hub
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push schemacrawler/chinook-database
+docker logout
