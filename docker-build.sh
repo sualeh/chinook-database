c0c3781f9ad015047277de778d31a538851a17af
Sualeh Fatehi <sualeh@hotmail.com>
2021-09-08 17:28:27 -0400

    Push all tags to Docker Hub

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-CHINOOK_DATABASE_VERSION=2.1.2
+CHINOOK_DATABASE_VERSION=v2.1.2
 
-echo "** Creating Chinook Database v$CHINOOK_DATABASE_VERSION Docker container"
+echo "** Creating Chinook Database $CHINOOK_DATABASE_VERSION Docker container"
 
 # Print Docker version
 pwd
@@ -11,10 +11,10 @@ docker version
 
 # Build Docker image
 docker build -t schemacrawler/chinook-database .
-docker tag schemacrawler/chinook-database schemacrawler/chinook-database:v$CHINOOK_DATABASE_VERSION
+docker tag schemacrawler/chinook-database schemacrawler/chinook-database:$CHINOOK_DATABASE_VERSION
 docker tag schemacrawler/chinook-database schemacrawler/chinook-database:latest
 
 # Deploy image to Docker Hub
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push schemacrawler/chinook-database
+docker push --all-tags schemacrawler/chinook-database
 docker logout
