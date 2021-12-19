f175d04811ff016a0399cc0bc6398a4a91eab30c
Sualeh Fatehi <sualeh@hotmail.com>
2021-12-18 21:59:10 -0500

    Explicit use of SQLite driver

--- src/main/java/us/fatehi/chinook_database/SQLiteGenerator.java
+++ src/main/java/us/fatehi/chinook_database/SQLiteGenerator.java
@@ -29,7 +29,10 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.sql.SQLException;
 
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+import org.sqlite.JDBC;
 
 public class SQLiteGenerator {
 
@@ -41,15 +44,15 @@ public class SQLiteGenerator {
       throws SQLException {
     requireNonNull(chinookDatabasePath, "No database path provided");
 
-    final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-    dataSource.setUrl("jdbc:sqlite:" + chinookDatabasePath);
+    final DataSource dataSource =
+        new SimpleDriverDataSource(new JDBC(), "jdbc:sqlite:" + chinookDatabasePath);
 
     ChinookDatabaseUtils.createChinookDatabase(sqlite, dataSource.getConnection());
 
     return chinookDatabasePath;
   }
 
-  public static void main(String[] args) throws Exception {
+  public static void main(final String[] args) throws Exception {
     final Path sqLiteChinookDatabasePath;
     if (args == null || args.length == 0) {
       sqLiteChinookDatabasePath = createSQLiteChinookDatabase();
