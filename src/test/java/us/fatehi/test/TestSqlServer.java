457a2909cbfabede096b1fd9326a30d23864ffe8
Sualeh Fatehi <sualeh@hotmail.com>
2022-12-26 10:16:59 -0500

    Update copyright

--- src/test/java/us/fatehi/test/TestSqlServer.java
+++ src/test/java/us/fatehi/test/TestSqlServer.java
@@ -2,7 +2,7 @@
 ========================================================================
 Chinook Database
 http://www.schemacrawler.com
-Copyright (c) 2020-2022, Sualeh Fatehi <sualeh@hotmail.com>.
+Copyright (c) 2020-2023, Sualeh Fatehi <sualeh@hotmail.com>.
 All rights reserved.
 ------------------------------------------------------------------------
 
